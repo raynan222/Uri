948e6d66a2b9ba7891fff59fe931da82f2fa2e70
raynan222 <38507894+raynan222@users.noreply.github.com>
2018-05-10 11:27:31 -0300

    Create 1054 - Sapo Dinâmico.c

---
+++ 1054 - Sapo Dinâmico.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+int main () {
+	int N,i,maiorida,maiorvolta,toggle,sida,svolta,T;
+	unsigned long long int D, M;
+	char tipo;
+	scanf ("%d",&T);
+	for (i=0;i<T;i++){
+		scanf ("%d %llu", &N, &D);
+		while (N--){
+			getchar();
+			scanf ("%c-%llu", &tipo, &M);
+				if (tipo=='B'){
+					if ((M-sida)>maiorida){
+						maiorida=(M-sida);
+						sida=m;
+					}
+					if ((M-svolta)>maiorvolta){
+						maiorvolta=(M-svolta);
+						svolta=m;
+					}
+					toggle=0;
+				}
+				if (tipo=='S'){
+					if(toggle%2==0){
+						if ((M-svolta)>maiorvolta){
+							maiorvolta=(M-svolta);
+							svolta=m;
+						}
+					}
+					else{
+						if ((M-sida)>maiorida){
+							maiorida=(M-sida);
+							sida=m;
+						}
+					}
+					toggle++;
+				}
+		}
+		if (D-sida>maiorida){
+			maiorida=D-sida;
+		}
+		if (D-svolta>maiorvolta){
+			maiorvolta=D-svolta;
+		}
+		printf ("Case %d: %llu\n", i+1,(maiorida>maiorvolta?maiorida;maiorvolta));
+	}
+	
+	return 0;
+}
