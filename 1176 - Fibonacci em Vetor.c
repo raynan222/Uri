5b8c7c83520021550ee1db693f182a817f9bc961
raynan222 <38507894+raynan222@users.noreply.github.com>
2019-01-30 20:43:10 -0300

    Create 1176 - Fibonacci em Vetor.c

---
+++ 1176 - Fibonacci em Vetor.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+void fibDin(int indice, long int vetFib[]);
+
+int main()
+{
+    long int vetFib[10000]={0,1}, numCaso, indiceDesejado, indiceMaisAlto=1;
+    
+    for(int i=2;i<10000;i++){
+        vetFib[i]=4;
+    }
+    
+    scanf("%d",&numCaso);
+    
+    while(numCaso--){
+        scanf("%d",&indiceDesejado);
+        
+        while(vetFib[indiceDesejado]==4){
+            fibDin(indiceMaisAlto+1, vetFib);
+            indiceMaisAlto++;
+        }
+        printf("Fib(%d) = %d\n", indiceDesejado, vetFib[indiceDesejado]);
+    }
+    
+    return 0;
+}
+
+void fibDin(int indice, long int vetFib[]){
+    vetFib[indice]=vetFib[indice-1]+vetFib[indice-2];
+}
+
+/* Codigo aprensenta "Compilation error" */
