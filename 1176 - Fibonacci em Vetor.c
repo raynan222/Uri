52c5f6ae82d2719317eed5cf13ad3efb5e1edcc4
raynan222 <38507894+raynan222@users.noreply.github.com>
2019-01-30 21:37:28 -0300

    Update 1176 - Fibonacci em Vetor.c

--- 1176 - Fibonacci em Vetor.c
+++ 1176 - Fibonacci em Vetor.c
@@ -5,9 +5,9 @@ void fibDin(int indice, long long int vetFib[]);
 
 int main()
 {
-    long long int vetFib[200]={0,1}, numCaso, indiceDesejado, indiceMaisAlto=1;
+    long long int vetFib[1000]={0,1}, numCaso, indiceDesejado, indiceMaisAlto=1;
     
-    for(int i=2;i<200;i++){
+    for(int i=2;i<1000;i++){
         vetFib[i]=4;
     }
     
@@ -20,7 +20,7 @@ int main()
             fibDin(indiceMaisAlto+1, vetFib);
             indiceMaisAlto++;
         }
-        printf("Fib(%llu) = %llu\n", indiceDesejado, vetFib[indiceDesejado]);
+        printf("Fib(%lld) = %llu\n", indiceDesejado, vetFib[indiceDesejado]);
     }
     return 0;
 }
